74138c0666f256341232867fb2bd0c6594a8c6ad
Jonathan <jono_95@yahoo.com>
2023-03-31 11:32:14 -0500

    models and controllers

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import PlayerList from './PlayerList';
+import Purpose from './Purpose';
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from './Header';
+import NavBar from './NavBar';
+import AddPlayer from './AddPlayer';
+
 
 function App() {
+
+  const [players, setPlayers] = useState([]) 
+
+  function addPlayer(newPlayer) {
+    const updatedPlayers = [...players, newPlayer]
+    setPlayers(updatedPlayers)
+  }
+
+  useEffect(()=> {
+    fetch("http://localhost:9292/players")
+    .then(r=> r.json())
+    .then(data => {
+      setPlayers(data)
+    })
+  }, [])
+
+  function removePlayer(id) {
+    const updatedPlayers = players.filter((player) => player.id !== id )
+    setPlayers(updatedPlayers)
+  }
+
+  function onDeleteAgent(agent) {
+    const player = players.find((player) => player.id === agent.player_id)
+    const updatedAgents = player.agents.filter((a)=> a.id !== agent.id)
+    const updatedPlayer = {...player, agents: updatedAgents}
+    const updatedPlayers = players.map((p) => p.id === player.id ? updatedPlayer : p)
+    setPlayers(updatedPlayers)
+  }
+
+  function onChangeAgent(data) {
+    const player = players.find((player) => player.id === data.player_id)
+    const updatedAgents = player.agents.map((a)=> a.id === data.id ? data : a)
+    const updatedPlayer = {...player, agents: updatedAgents}
+    const updatedPlayers = players.map((p) => p.id === player.id ? updatedPlayer : p)
+    setPlayers(updatedPlayers)
+  }
+
+  function onAddAgent(data) {
+    const player = players.find((player) => player.id === data.player_id)
+    player.agents.push(data)
+  }
+   
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <div>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Purpose />}/>
+            <Route path="/roster" element={<PlayerList players={players} onRemovePlayer={removePlayer} onDeleteAgent={onDeleteAgent} onChangeAgent={onChangeAgent} onAddAgent={onAddAgent}/>}/>
+            <Route path="/add" element={<AddPlayer onAddPlayer={addPlayer}/>}/>
+          </Routes>
+      </div>
     </div>
   );
 }
